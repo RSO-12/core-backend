29caa5463531b118430324514a0209360f9df940
Janez Sedeljšak <janez.sedeljsak@gmail.com>
2023-11-01 02:20:50 +0100

    - workflow for auto publish docker image - add unit tests and workflow

---
+++ api/src/test/java/jb/workclock/test/MockTest.java
@@ -0,0 +1,13 @@
+package jb.workclock.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MockTest {
+
+    @Test
+    public void testAddition() {
+
+        Assertions.assertEquals(1 + 2, 3);
+    }
+}
