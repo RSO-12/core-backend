8e9aa348d2943ce953f1c0c94c50d16c6ce7c417
Janez Sedeljšak <janez.sedeljsak@gmail.com>
2023-11-10 22:34:23 +0100

    docker compose all services running

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,2 +1,3 @@
 INSERT INTO users (name, gmail, password) VALUES ('Janez Sedeljsak', 'janez@gmail.com', '_');
+INSERT INTO users (name, gmail, password) VALUES ('Lorem Ipsum', 'lorem@gmail.com', '_');
 INSERT INTO teams (name) VALUES ('RSO projekt');
