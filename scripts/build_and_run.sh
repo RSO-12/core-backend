5376554d397726bbd957d09c931fd7ff6605a119
Janez Sedeljšak <janez.sedeljsak@gmail.com>
2024-01-09 17:45:45 +0100

    - add stats to ci/cd - livenessProbes - adjusted scripts

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -3,16 +3,19 @@
 path_core="../"
 path_services="../../workclock-services"
 path_socket="../../workclock-socket"
+path_stats="../../workclock-stats"
 
 image_core="janezs12/workclock-core"
 image_services="janezs12/workclock-services"
 image_socket="janezs12/workclock-socket-v3"
+image_stats="janezs12/workclock-stats"
 
 
 # Build Docker images
 docker build -t "$image_core" "$path_core"
 docker build -t "$image_services" "$path_services"
 docker build -t "$image_socket" "$path_socket"
+docker build -t "$image_stats" "$path_stats"
 
 echo "Docker images built successfully: $image_core, $image_services, $image_socket"
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,14 +3,17 @@
 path_core="../"
 path_services="../../workclock-services"
 path_socket="../../workclock-socket"
+path_stats="../../workclock-stats"
 
 image_core="janezs12/workclock-core"
 image_services="janezs12/workclock-services"
 image_socket="janezs12/workclock-socket-v3"
+image_stats="janezs12/workclock-stats"
 
 # Push Docker images
 docker push "$image_core"
 docker push "$image_services"
 docker push "$image_socket"
+docker push "$image_stats"
 
-echo "Docker images published successfully: $image_core, $image_services, $image_socket"
+echo "Docker images published successfully: $image_core, $image_services, $image_socket, $image_stats"
